55a9d4e2b0a976914fcd7d7b59c1d81f63a81b6e
Gaurav Sahu <GauravSahu@users.noreply.github.com>
2015-10-15 16:29:56 +0530

    search method

--- odoo.js
+++ odoo.js
@@ -198,6 +198,14 @@ module.exports =  (function (){
 		};
     	this.rpc('/web/dataset/call',cb,params);
     }
+    odoo.prototype._search_read = function(model,cb,args,method){	
+    	var params = {
+	    	"model": model,
+        	"method" : method,
+	    	"args": args
+		};
+    	this.rpc('/web/dataset/call',cb,params);
+    }
     odoo.prototype._delete = function(model,cb,method,args){
     	
     	var params = {
